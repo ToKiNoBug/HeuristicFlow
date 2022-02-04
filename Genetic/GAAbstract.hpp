d5bf4de6cd43e48c394f86a0445833552a22a593
时之虫 <62002934+ToKiNoBug@users.noreply.github.com>
2022-02-04 22:31:46 +0800

    Update GAAbstract.hpp

--- Genetic/GAAbstract.hpp
+++ Genetic/GAAbstract.hpp
@@ -1,8 +1,30 @@
+/*
+ Copyright © 2022  TokiNoBug
+This file is part of OptimTemplates.
+
+    OptimTemplates is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OptimTemplates is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OptimTemplates.  If not, see <https://www.gnu.org/licenses/>.
+
+*/
+
 #ifndef OptimT_GAABSTRACT_HPP
 #define OptimT_GAABSTRACT_HPP
 
 namespace OptimT {
 
+/**
+ * @brief The GAAbstract class declares 4 operator functions and related reloaded functions.
+ */
 template<typename Var_t,typename Fitness_t,class Args_t>
 class GAAbstract
 {
