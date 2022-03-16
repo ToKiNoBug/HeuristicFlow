1ba3cfc1ed6ebf3aa51c2b5b6040a79cbebf1d3b
ToKiNoBug <tokinobug@163.com>
2022-03-16 22:00:07 +0800

    add license declerations

--- HeuristicFlow/src/PSO/PSOParameterPack.hpp
+++ HeuristicFlow/src/PSO/PSOParameterPack.hpp
@@ -1,3 +1,22 @@
+/*
+ Copyright © 2022  TokiNoBug
+This file is part of Heuristic.
+
+    Heuristic is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Heuristic is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Heuristic.  If not, see <https://www.gnu.org/licenses/>.
+
+*/
+
 #ifndef Heu_PSOPARAMETERPACK_HPP
 #define Heu_PSOPARAMETERPACK_HPP
 
