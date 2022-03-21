f7e0dac0781b882999e19bd05b6260920fae2fba
ToKiNoBug <tokinobug@163.com>
2022-03-21 11:05:14 +0800

    Update Randoms.hpp

--- HeuristicFlow/src/Global/Randoms.hpp
+++ HeuristicFlow/src/Global/Randoms.hpp
@@ -46,6 +46,7 @@ inline std::random_device& global_random_device() {
  */
 inline std::mt19937& global_mt19937() {
 #ifdef EIGEN_HEU_std_random_device_NOT_RELIABLE
+  // Use a hash value of system clock as the random seed
   static auto now = std::chrono::system_clock::now();
   static std::time_t time = std::chrono::system_clock::to_time_t(now);
   static uint32_t seed = std::hash<std::time_t>()(time);
@@ -59,26 +60,56 @@ inline std::mt19937& global_mt19937() {
 
 }  // namespace internal
 
-/// uniform random number in range [0,1)
+/**
+ * \brief Uniform random number (double) in range [0,1)
+ *
+ * \return double random number
+ */
 inline double ei_randD() {
   static std::uniform_real_distribution<double> rnd(0, 1);
   return rnd(internal::global_mt19937());
 }
 
-/// uniform random number in range [min,max)
+/**
+ * \brief Uniform random number (double) in range [min,max)
+ *
+ * \param min Minimum value
+ * \param max Maximum value
+ * \return double random number
+ */
 inline double ei_randD(const double min, const double max) { return (max - min) * ei_randD() + min; }
 
+/**
+ * \brief Uniform random number (float) in range [0,1)
+ *
+ * \return double random number
+ */
 inline float ei_randF() {
   static std::uniform_real_distribution<float> rnd(0, 1);
   return rnd(internal::global_mt19937());
 }
 
+/**
+ * \brief Uniform random index in range [0,size)
+ *
+ * \tparam int_t Type of integer
+ * \param size Maxixmum size value
+ * \return int_t Random index in range [0,size-1]
+ */
 template <typename int_t>
 inline int_t ei_randIdx(int_t size) {
   static_assert(std::is_integral<int_t>::value, "int_t must be integer");
   return int_t(ei_randF() * size);
 }
 
+/**
+ * \brief Uniform random index in range [min,max_plus_1)
+ *
+ * \tparam int_t Type of integer
+ * \param min Minimun value
+ * \param max_plus_1 One plus the maximum value
+ * \return int_t Random index in range [min,max_plus_1 -1]
+ */
 template <typename int_t>
 inline int_t ei_randIdx(int_t min, int_t max_plus_1) {
   static_assert(std::is_integral<int_t>::value, "int_t must be integer");
