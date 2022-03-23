a1985df9494bea2efe5b5574cfdff3646a38b8b2
ToKiNoBug <tokinobug@163.com>
2022-03-23 20:31:04 +0800

    document NSGABase

--- HeuristicFlow/src/Genetic/NSGABase.hpp
+++ HeuristicFlow/src/Genetic/NSGABase.hpp
@@ -17,6 +17,23 @@ namespace Eigen {
 
 namespace internal {
 
+/**
+ * \ingroup HEU_Genetic
+ * \class NSGABase
+ * \brief Base class for nondominated sorting series of genetic algorithms, like NSGA2 and NSGA3.
+ *
+ * This class implements nondominated sorting, which is common in NSGA2 and NSGA3.
+ *
+ * \tparam Var_t
+ * \tparam ObjNum
+ * \tparam fOpt
+ * \tparam rOpt
+ * \tparam Args_t
+ * \tparam _iFun_
+ * \tparam _fFun_
+ * \tparam _cFun_
+ * \tparam _mFun_
+ */
 template <typename Var_t, int ObjNum, FitnessOption fOpt, RecordOption rOpt, class Args_t,
           typename GAAbstract<Var_t, Eigen::Array<double, ObjNum, 1>, Args_t>::initializeFun _iFun_,
           typename GAAbstract<Var_t, Eigen::Array<double, ObjNum, 1>, Args_t>::fitnessFun _fFun_,
@@ -27,28 +44,40 @@ class NSGABase : public MOGABase<Var_t, ObjNum, fOpt, rOpt, Args_t, _iFun_, _fFu
   using Base_t = MOGABase<Var_t, ObjNum, fOpt, rOpt, Args_t, _iFun_, _fFun_, _cFun_, _mFun_>;
 
  public:
-  NSGABase(){};
-  virtual ~NSGABase(){};
-
   EIGEN_HEU_MAKE_GABASE_TYPES(Base_t)
   using Fitness_t = typename Base_t::Fitness_t;
 
   /**
-   * @brief basical unit for NS
+   * \brief Basical unit for NS
    *
+   * This struct stores basic informations of a gene used in nondominated sorting.
    */
   struct infoUnitBase {
    public:
+    /**
+     * \brief A copy of fitness to boost nondominated sorting.
+     *
+     */
     Fitness_t fitnessCache;
-    /** @brief Genes in population that strong domain this gene
+
+    /**
+     * \brief Genes in population that strong dominate this gene
+     *
+     * Value 0 means it's a member of the pareto
+     * front.
      */
     size_t domainedByNum;
 
-    /** @brief iterator to related gene
+    /**
+     * \brief Iterator to related gene
      */
     GeneIt_t iterator;
   };  //  infoUnitBase
 
+  /**
+   * \brief Reimplemented
+   *
+   */
   void initializePop() {
     sortSpace.clear();
     sortSpace.reserve(2 * this->_option.populationSize);
@@ -56,14 +85,37 @@ class NSGABase : public MOGABase<Var_t, ObjNum, fOpt, rOpt, Args_t, _iFun_, _fFu
   }
 
  protected:
+  /**
+   * \brief A vector of infoUnitBase*. This vector is where NS-sorting will take place since copying swaping pointers
+   * is much faster than swaping structs.
+   *
+   */
   std::vector<infoUnitBase*> sortSpace;
+
+  /**
+   * \brief A list of vectors that divides the current population into several nondominated layers.
+   *
+   */
   std::list<std::vector<infoUnitBase*>> pfLayers;
 
+  /**
+   * \brief Compare function used to sort by dominatedNum in ascend.
+   *
+   * \param A The first input
+   * \param B The second input
+   * \return true A is dominated by less genes than B
+   * \return false A is dominated by more or equal genes than B
+   */
   static bool sortByDominatedNum(const infoUnitBase* A, const infoUnitBase* B) {
     return (A->domainedByNum) < (B->domainedByNum);
   }
 
   // calculate domainedByNum
+
+  /**
+   * \brief Function to calculate infoUnitBase::domainedByNum of a population.
+   *
+   */
   virtual void calculateDominatedNum() {
     const size_t popSizeBefore = sortSpace.size();
 #ifdef EIGEN_HAS_OPENMP
@@ -89,8 +141,12 @@ class NSGABase : public MOGABase<Var_t, ObjNum, fOpt, rOpt, Args_t, _iFun_, _fFu
     }
 #endif
 
-  }  // calculateDominatedNum()
+  }  // calculateDominatedNum
 
+  /**
+   * \brief Divide the sorted population (is sortSpace) into a few non-dominated layers.
+   *
+   */
   virtual void divideLayers() {
     std::sort(sortSpace.begin(), sortSpace.end(), sortByDominatedNum);
     pfLayers.clear();
@@ -106,6 +162,10 @@ class NSGABase : public MOGABase<Var_t, ObjNum, fOpt, rOpt, Args_t, _iFun_, _fFu
     }
   }
 
+  /**
+   * \brief Update the pareto front
+   *
+   */
   void updatePF(const infoUnitBase** pfs, const size_t curFrontSize) {
     this->_pfGenes.clear();
     for (size_t i = 0; i < curFrontSize; i++) {
