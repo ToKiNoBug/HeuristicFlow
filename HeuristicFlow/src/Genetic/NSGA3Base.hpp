81bdd90ed8b1f5ca6ba6919de2938b6a0b15afc4
ToKiNoBug <tokinobug@163.com>
2022-03-24 21:43:14 +0800

    document NSGA3Base

--- HeuristicFlow/src/Genetic/NSGA3Base.hpp
+++ HeuristicFlow/src/Genetic/NSGA3Base.hpp
@@ -16,13 +16,44 @@
 namespace Eigen {
 
 /**
- * @brief Layers of Reference points
+ * \ingroup HEU_Genetic
+ * \brief Different options of reference points.
  *
+ * Here's two ways to generate reference points: the Das and Dennis' systematic approach generates a single layer, while
+ * Deb and Jain’s method(based on the previous method) generates two layers of RS.
+ *
+ * The first method places RPs on a M-1 dim hyperplane (where M is the number of objectives) equally inclined on all
+ * axes and has a intercept of 1 to all objectives. This method requires relatively higher precision and generates great
+ * numbers of RPs for high-dimensional MO problems. This is detrimental to preformance.
+ *
+ * While the Deb and Jain’s method adds a inner hyperplane(inside layer) whose intercept has smaller value than the
+ * boundary layer(normalized hyperplane). Then the algorithm places RPs on two layers with relatively small precision.
+ * It works with much fewer RPs.
  */
-enum ReferencePointOption { SINGLE_LAYER, DOUBLE_LAYER };
+enum ReferencePointOption {
+  SINGLE_LAYER,  ///< Das and Dennis' method
+  DOUBLE_LAYER   ///< Deb and Jain's method
+};
 
 namespace internal {
 
+/**
+ * \ingroup HEU_Genetic
+ * \class NSGA3Base
+ * \brief Internal base class for NSGA3.
+ *
+ * This class implements different methods to make reference points.
+ *
+ * \tparam Var_t
+ * \tparam ObjNum
+ * \tparam rOpt
+ * \tparam rpOpt Reference point method.
+ * \tparam Args_t
+ * \tparam _iFun_
+ * \tparam _fFun_
+ * \tparam _cFun_
+ * \tparam _mFun_
+ */
 template <typename Var_t, int ObjNum, RecordOption rOpt, ReferencePointOption rpOpt, class Args_t,
           typename GAAbstract<Var_t, Eigen::Array<double, ObjNum, 1>, Args_t>::initializeFun _iFun_,
           typename GAAbstract<Var_t, Eigen::Array<double, ObjNum, 1>, Args_t>::fitnessFun _fFun_,
@@ -34,15 +65,39 @@ class NSGA3Base : public NSGA3Abstract<Var_t, ObjNum, rOpt, Args_t, _iFun_, _fFu
  public:
   EIGEN_HEU_MAKE_NSGA3ABSTRACT_TYPES(Base_t)
 
+  /**
+   * \brief Get the precision of RPs
+   *
+   * \note This function only exists when single layer.
+   *
+   * \return size_t Precision
+   */
   size_t referencePointPrecision() const { return _precision; }
 
+  /**
+   * \brief Set the Reference Point Precision object
+   *
+   * \note This function only exists when single layer.
+   *
+   * \param p Precison value, not less than 2.
+   */
   void setReferencePointPrecision(size_t p) { _precision = p; }
 
+  /**
+   * \brief Get the number of RPs
+   *
+   * \return size_t Number of RPs
+   */
   size_t referencePointCount() const { return NchooseK(_precision + this->objectiveNum() - 1, _precision); }
 
  protected:
-  size_t _precision;
+  size_t _precision;  ///< Precision of RPs in single layer. Only exists when rpOpt==SINGLE_LAYER
 
+  /**
+   * \brief Compute reference points
+   *
+   * This function is reloaded with different rpOpt.
+   */
   void makeReferencePoses() {
     this->referencePoses.resize(this->objectiveNum(), referencePointCount());
     std::vector<Fitness_t> rfP;
@@ -74,10 +129,32 @@ class NSGA3Base<Var_t, ObjNum, rOpt, DOUBLE_LAYER, Args_t, _iFun_, _fFun_, _cFun
     _outerPrecision = 4;
   };
 
+  /**
+   * \brief Get the precison of inner layer
+   *
+   * \note This function only exists with double layers.
+   *
+   * \return size_t Precision of inner layer
+   */
   size_t innerPrecision() const { return _innerPrecision; }
 
+  /**
+   * \brief Get the precison of outer layer
+   *
+   * \note This function only exists with double layers.
+   *
+   * \return size_t Precision of outer layer
+   */
   size_t outerPrecision() const { return _outerPrecision; }
 
+  /**
+   * \brief Set the precison of inner and outer layers
+   *
+   * \note This function only exists with double layers.
+   *
+   * \param i Inner layer precision
+   * \param o Outer layer precision
+   */
   void setReferencePointPrecision(size_t i, size_t o) {
     assert(i >= 2);
     assert(o >= 2);
@@ -91,8 +168,8 @@ class NSGA3Base<Var_t, ObjNum, rOpt, DOUBLE_LAYER, Args_t, _iFun_, _fFun_, _cFun
   }
 
  protected:
-  size_t _innerPrecision;
-  size_t _outerPrecision;
+  size_t _innerPrecision;  ///< Precision of inner layer
+  size_t _outerPrecision;  ///< Precision of outer layer
 
   void makeReferencePoses() {
     this->referencePoses.resize(this->objectiveNum(), referencePointCount());
