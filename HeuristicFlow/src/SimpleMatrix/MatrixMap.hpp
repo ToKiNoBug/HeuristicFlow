195e3ad078a1b25d68f638fa6636855fed1eb4dc
TokiNoBug <tokinobug@163.com>
2022-05-21 15:26:16 +0800

    removed all declerations

--- HeuristicFlow/src/SimpleMatrix/MatrixMap.hpp
+++ HeuristicFlow/src/SimpleMatrix/MatrixMap.hpp
@@ -1,21 +1,3 @@
-/*
- Copyright © 2022  TokiNoBug
-This file is part of Heuristic.
-
-    Heuristic is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    Heuristic is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with Heuristic.  If not, see <https://www.gnu.org/licenses/>.
-
-*/
 
 #ifndef Heu_MATRIXMAP_HPP
 #define Heu_MATRIXMAP_HPP
