0dcbb18bc2ee1eb7a6084d1bb55a49f12b2a5be2
TokiNoBug <tokinobug@163.com>
2022-06-18 19:27:03 +0800

    tested and refined poolVector

--- test/poolVector.cpp
+++ test/poolVector.cpp
@@ -1,6 +1,8 @@
 #include <HeuristicFlow/SimpleMatrix>
-
+#include <HeuristicFlow/Global>
+#include <cmath>
 #include <iostream>
+#include <vector>
 using std::cout;
 using std::endl;
 
@@ -13,8 +15,30 @@ class testScalar {
   double value;
 };
 
+class counterScalar {
+ public:
+  counterScalar() { createdNum++; }
+  counterScalar(const counterScalar &) { createdNum++; }
+  // counterScalar(counterScalar &&) { createdNum++; }
+  ~counterScalar() { createdNum--; }
+  static int createdNum;
+  char val[3];
+};
+
+int counterScalar::createdNum = 0;
+
 int main() {
-  cout << __LINE__ << endl;
-  heu::poolVector<testScalar> vec;
-  cout << __LINE__ << endl;
+  constexpr int loopN = 1e4;
+  const int mallocMax = std::pow(2, 12);
+  auto vec = new heu::poolVector<counterScalar>;
+  // auto vec = new std::vector<counterScalar>;
+  //   heu::poolVector<counterScalar> vec;
+  cout << "createdNum=" << counterScalar::createdNum << endl;
+  for (int loop = 0; loop < loopN; loop++) {
+    // vec->push_back();
+    vec->reserve(heu::randIdx(mallocMax));
+    //  vec->clear();
+  }
+  delete vec;
+  cout << "createdNum=" << counterScalar::createdNum << endl;
 }
