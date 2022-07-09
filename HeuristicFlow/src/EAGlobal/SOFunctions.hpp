3cdf128cc4520853f80aec70465b446e00895e4d
TokiNoBug <tokinobug@163.com>
2022-07-09 09:40:36 +0800

    add GPL declarations

--- HeuristicFlow/src/EAGlobal/SOFunctions.hpp
+++ HeuristicFlow/src/EAGlobal/SOFunctions.hpp
@@ -1,3 +1,22 @@
+/*
+ Copyright © 2021-2022  TokiNoBug
+This file is part of HeuristicFlow.
+
+    HeuristicFlow is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    HeuristicFlow is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with HeuristicFlow.  If not, see <https://www.gnu.org/licenses/>.
+
+*/
+
 #ifndef HEU_SOFUNCTIONS_HPP
 #define HEU_SOFUNCTIONS_HPP
 
